a690a5a9e17922107126c2e6a905f4b8196a8ed6
M.T <90578585+tmg-m@users.noreply.github.com>
2026-06-01 19:17:16 +0200

    Home page configuration x localhost 3000 x routes to home

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,9 +1,6 @@
-import { Head } from '@inertiajs/react';
-
-export default function Welcome() {
+export default function Home() {
     return (
         <>
-            <Head title="Welcome" />
             <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
                 <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
                     <main className="flex w-full max-w-[335px] flex-col-reverse lg:max-w-4xl lg:flex-row">
